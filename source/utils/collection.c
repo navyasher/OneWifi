1283dd8711bb9032602570491dad7c1539aeaa78
navyasher <navyasheregar@comcast.com>
2026-02-01 08:37:55 +0530

    removing free data

--- source/utils/collection.c
+++ source/utils/collection.c
@@ -379,9 +379,6 @@ void  hash_map_cleanup(hash_map_t *map)
         tmp = e->next;
         he = (hash_element_t *) e->data;
         if(he != NULL) {
-            if (he->data != NULL) {
-                free(he->data);
-            }
             if (he->key != NULL) {
                 free(he->key);
             }
