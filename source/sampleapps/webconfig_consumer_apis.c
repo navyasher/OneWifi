bf4eaacf8b75a76bb10cfe3c3269bdda3ef911f2
navyasher <navyasheregar@comcast.com>
2026-04-03 12:40:24 +0530

    test

--- source/sampleapps/webconfig_consumer_apis.c
+++ source/sampleapps/webconfig_consumer_apis.c
@@ -18,6 +18,7 @@
  **************************************************************************/
 
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include "const.h"
 #include "wifi_hal.h"
@@ -752,19 +753,43 @@ void test_null_subdoc_change(webconfig_consumer_t *consumer)
     if (str != NULL) {
         free(str);
     }
+        
+    free(data);
+    data = NULL;
+}
+static int secure_rand_mod(int mod)
+{
+    int fd;
+    unsigned int val;
+ 
+    fd = open("/dev/urandom", O_RDONLY);
+    if (fd < 0) {
+        return 0;  // safe fallback
+    }
+ 
+    if (read(fd, &val, sizeof(val)) != sizeof(val)) {
+        close(fd);
+        return 0;
+    }
+ 
+    close(fd);
+    return val % mod;
 }
 
 void test_mesh_sta_subdoc_change(webconfig_consumer_t *consumer)
 {
     webconfig_subdoc_data_t data;
     webconfig_error_t ret=webconfig_error_none;
-    time_t t;
 
     char *str;
     str = NULL;
 
-    memset(&data, 0, sizeof(webconfig_subdoc_data_t));
-    srand((unsigned) time(&t));
+    data = (webconfig_subdoc_data_t*)malloc(sizeof(webconfig_subdoc_data_t));
+    if (data == NULL) {
+        printf("%s:%d: Failed to allocate memory\n", __func__, __LINE__);
+        return;
+    }
+    memset(data, 0, sizeof(webconfig_subdoc_data_t));
 
     printf("%s:%d: current time:%llu\n", __func__, __LINE__, get_current_time_ms());
     if (enable_ovsdb == true) {
@@ -781,13 +806,13 @@ void test_mesh_sta_subdoc_change(webconfig_consumer_t *consumer)
                 printf("%s:%d: vap_info is NULL \n", __func__, __LINE__);
                 return;
             }
-            vap_info->u.sta_info.scan_params.period = rand() % 10;
-            vap_info = get_wifi_radio_vap_info(&data.u.decoded.radios[1], "mesh_sta");
+            vap_info->u.sta_info.scan_params.period = secure_rand_mod(10);
+            vap_info = get_wifi_radio_vap_info(&data->u.decoded.radios[1], "mesh_sta");
             if (vap_info == NULL) {
                 printf("%s:%d: vap_info is NULL \n", __func__, __LINE__);
                 return;
             }
-            vap_info->u.sta_info.scan_params.period = rand() % 10;
+            vap_info->u.sta_info.scan_params.period = secure_rand_mod(10);
         }
 
         // clearing the descriptor and raw json data
@@ -830,7 +855,6 @@ void test_mesh_subdoc_change(webconfig_consumer_t *consumer)
     webconfig_subdoc_data_t data;
     webconfig_error_t ret=webconfig_error_none;
     char test_mac[18];
-    time_t t;
     rdk_wifi_vap_info_t *rdk_vap;
     mac_address_t mac;
     acl_entry_t *acl_entry;
@@ -838,9 +862,13 @@ void test_mesh_subdoc_change(webconfig_consumer_t *consumer)
     char *str;
     str = NULL;
 
-    memset(&data, 0, sizeof(webconfig_subdoc_data_t));
-    srand((unsigned) time(&t));
-    snprintf(test_mac, sizeof(test_mac), "%02x:%02x:%02x:%02x:%02x:%02x", 0xaa, 0xbb,0xcc,0xaa, rand() % 25, rand() % 50);
+    data = (webconfig_subdoc_data_t*)malloc(sizeof(webconfig_subdoc_data_t));
+    if (data == NULL) {
+        printf("%s:%d: Failed to allocate memory\n", __func__, __LINE__);
+        return;
+    }
+    memset(data, 0, sizeof(webconfig_subdoc_data_t));
+    snprintf(test_mac, sizeof(test_mac), "%02x:%02x:%02x:%02x:%02x:%02x", 0xaa, 0xbb,0xcc,0xaa, secure_rand_mod(25), secure_rand_mod(50));
 
     printf("%s:%d: current time:%llu\n", __func__, __LINE__, get_current_time_ms());
     if (enable_ovsdb == true) {
@@ -934,15 +962,18 @@ void test_macfilter_subdoc_change(webconfig_consumer_t *consumer)
     rdk_wifi_vap_info_t *rdk_vap;
     mac_address_t mac;
     acl_entry_t *acl_entry;
-    time_t t;
 
     char *str;
     str = NULL;
 
-    memset(&data, 0, sizeof(webconfig_subdoc_data_t));
-    srand((unsigned) time(&t));
+    data = (webconfig_subdoc_data_t*)malloc(sizeof(webconfig_subdoc_data_t));
+    if (data == NULL) {
+        printf("%s:%d: Failed to allocate memory\n", __func__, __LINE__);
+        return;
+    }
+    memset(data, 0, sizeof(webconfig_subdoc_data_t));
 
-    snprintf(test_mac, sizeof(test_mac), "%02x:%02x:%02x:%02x:%02x:%02x", 0xaa, 0xbb,0xcc,0xdd, rand() % 25, rand() % 50);
+    snprintf(test_mac, sizeof(test_mac), "%02x:%02x:%02x:%02x:%02x:%02x", 0xaa, 0xbb,0xcc,0xdd, secure_rand_mod(25), secure_rand_mod(50));
 
     printf("%s:%d: current time:%llu\n", __func__, __LINE__, get_current_time_ms());
     if (enable_ovsdb == true) {
