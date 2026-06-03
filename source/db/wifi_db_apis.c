3cbf932926ca98fc051e38fbd1d455ef21fbf65f
navyasher <navyasheregar@comcast.com>
2026-06-03 16:27:29 +0530

    removing version check for loginterval

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -4883,9 +4883,9 @@ static void wifidb_global_config_upgrade()
     if (g_wifidb->db_version == 0) {
         return;
     }
-    if (g_wifidb->db_version < ONEWIFI_DB_VERSION_LOGINTERVAL_FLAG) {
-        wifi_util_dbg_print(WIFI_DB, "%s:%d upgrade global config, old db version %d \n", __func__,
-            __LINE__, g_wifidb->db_version);
+    // if (g_wifidb->db_version < ONEWIFI_DB_VERSION_LOGINTERVAL_FLAG) {
+    //     wifi_util_dbg_print(WIFI_DB, "%s:%d upgrade global config, old db version %d \n", __func__,
+    //         __LINE__, g_wifidb->db_version);
 
         memset(strValue, 0, sizeof(strValue));
         str = (char *)p_ccsp_desc->psm_get_value_fn(WhixLoginterval, strValue, sizeof(strValue));
@@ -4898,7 +4898,7 @@ static void wifidb_global_config_upgrade()
             wifi_util_error_print(WIFI_DB, ":%s:%d str value for whix_log_interval is null \r\n",
                 __func__, __LINE__);
         }
-    }
+    // }
 
     if (g_wifidb->db_version < ONEWIFI_DB_VERSION_CHUTILITY_LOGINTERVAL_FLAG) {
         wifi_util_dbg_print(WIFI_DB, "%s:%d upgrade global config, old db version %d \n", __func__,
@@ -8706,6 +8706,9 @@ int wifi_db_update_global_config(wifi_global_param_t *global_cfg)
     get_wifidb_obj()->desc.init_global_config_default_fn(global_cfg);
 
     memset(strValue, 0, sizeof(strValue));
+
+wifi_util_dbg_print(WIFI_MGR,"NTesting wifi_db_update_global_config function\n");
+
 #ifndef NEWPLATFORM_PORT
     str = p_ccsp_desc->psm_get_value_fn(WiFivAPStatsFeatureEnable, strValue, sizeof(strValue));
     if (str != NULL) {
